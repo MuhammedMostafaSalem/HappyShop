095d28ebceec650da0ed65288cdb27c6cb802387
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2022-11-27 18:44:28 +0200

    add slider component to HomePage file

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Slider from '../../components/Home/Slider/Slider';
 
 const HomePage = () => {
     return (
-        <div>HomePage</div>
+        <div>
+            <Slider/>
+        </div>
     )
 }
 
