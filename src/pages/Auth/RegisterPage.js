b72a2f919dff960c3dfff267c14e64a568e4cf2a
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2022-12-01 21:14:00 +0200

    Add RegisterComponent to RegisterPage file

---
+++ src/pages/Auth/RegisterPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import RegisterComponent from './../../components/Auth/Register/Register';
+
+const RegisterPage = () => {
+    return (
+        <div style={{ minHeight: "505px" }}>
+            <RegisterComponent/>
+        </div>
+    )
+}
+
+export default RegisterPage
