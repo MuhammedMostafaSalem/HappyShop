e103f8a4787a36d2ef581430637efa1a4e1116ff
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2022-12-01 21:13:20 +0200

    Add LoginComponent to LoginPage file

--- src/pages/Auth/LoginPage/LoginPage.js
+++ src/pages/Auth/LoginPage/LoginPage.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Login from './../../../components/Auth/Login/Login';
 
 const LoginPage = () => {
     return (
-        <div style={{ minHeight: "505px" }}>LoginPage</div>
+        <div style={{ minHeight: "505px" }}>
+            <Login/>
+        </div>
     )
 }
 
