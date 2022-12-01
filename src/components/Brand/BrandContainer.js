0ed3eb33318abaf1f8337dac82ed393997744137
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2022-12-01 23:32:53 +0200

    Create BrandContainerComponent

--- src/components/Brand/BrandContainer.js
+++ src/components/Brand/BrandContainer.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import brand1 from "../../../images/brand1.png";
-import brand2 from "../../../images/brand2.png";
-import brand3 from "../../../images/brand3.png";
+import brand1 from "../../images/brand1.png";
+import brand2 from "../../images/brand2.png";
+import brand3 from "../../images/brand3.png";
+import BrandCard from './BrandCard';
+import { Container, Row } from 'react-bootstrap';
+import Subtitle from './../Utility/subTitle/Subtitle';
 
 const BrandContainer = () => {
     return (
         <Container>
-            <div className="admin-content-text mt-2 ">كل الماركات</div>
+            <Subtitle title='كل الماركات' />
             <Row className='my-1 d-flex justify-content-between'>
                 <BrandCard img={brand1} />
                 <BrandCard img={brand2} />
