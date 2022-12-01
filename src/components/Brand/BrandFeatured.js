be54d78099a84024993ff99e8921419e7c85f1b3
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2022-12-01 23:31:57 +0200

    Add pathText Link

--- src/components/Brand/BrandFeatured.js
+++ src/components/Brand/BrandFeatured.js
@@ -6,10 +6,10 @@ import brand1 from "../../images/brand1.png";
 import brand2 from "../../images/brand2.png";
 import brand3 from "../../images/brand3.png";
 
-const BrandFeatured = ({title , btnTitle}) => {
+const BrandFeatured = ({title , btnTitle , pathText}) => {
     return (
         <Container className='py-3'>
-            <Subtitle title={title} btnTitle={btnTitle} />
+            <Subtitle title={title} btnTitle={btnTitle} pathText='/AllBrand' />
             <Row className='my-1 d-flex justify-content-between'>
                 <BrandCard img={brand1} />
                 <BrandCard img={brand2} />
