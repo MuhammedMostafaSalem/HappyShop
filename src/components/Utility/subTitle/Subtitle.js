93d1c0d6dfe0192066d6991253ae2cf576003d5a
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2022-12-01 23:00:23 +0200

    Add Link Router for pathtext

--- src/components/Utility/subTitle/Subtitle.js
+++ src/components/Utility/subTitle/Subtitle.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../subTitle/SubTitle.css'
 
-const Subtitle = ({title , btnTitle}) => {
+const Subtitle = ({title , btnTitle , pathText}) => {
     return (
         <div className="d-flex justify-content-between pt-4">
             <div className='sub-tile'>{title}</div>
             {
-                btnTitle ? ( <div className='shopping-now'>{btnTitle}</div> ) : null
+                btnTitle ? ( 
+                    <Link to={`${pathText}`} className='text-decoration-none'>
+                        <div className='shopping-now'>{btnTitle}</div>
+                    </Link>
+                ) : null
             }
         </div>
     )
