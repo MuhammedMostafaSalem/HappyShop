8e00a282d905b448a7f223ec111c908fa8a59750
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2022-12-03 01:44:38 +0200

    Add alt for img in CategoryCard

--- src/components/Category/CtategoryCard/CtategoryCard.js
+++ src/components/Category/CtategoryCard/CtategoryCard.js
@@ -7,7 +7,7 @@ const CtategoryCard = ({background , img , title}) => {
         <Col lg='2' md='4' sm='6' xs='6' className='my-4 d-flex justify-content-around'>
             <div className='allCard mb-3'>
                 <div className="categoty-card" style={{backgroundColor: `${background}`}}></div>
-                <img src={img} className="categoty-card-img" />
+                <img src={img} alt='' className="categoty-card-img" />
                 <p className="categoty-card-text my-2">{title}</p>
             </div>
         </Col>
