30b7bb6de2c6030bbec3cf43d5c94c66b56f4173
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2022-12-03 02:40:39 +0200

    Create CartPage

--- src/pages/Cart/CartPage.js
+++ src/pages/Cart/CartPage.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import CartItem from './../../components/Cart/CartItem/CartItem';
+import CartCheckout from './../../components/Cart/CartCheckout/CartCheckout';
 
 const CartPage = () => {
     return (
-        <div style={{ minHeight: "505px" }}>CartPage</div>
+        <Container style={{ minHeight: "505px" }}>
+            <Row>
+                <div className='cart-title mt-4'>عربة التسوق</div>
+            </Row>
+            <Row className='d-flex justify-content-center'>
+                <Col md="9" sm='12'>
+                    <CartItem/>
+                    <CartItem/>
+                    <CartItem/>
+                </Col>
+                <Col md='3' sm='6'>
+                    <CartCheckout/>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
