17a9df83ab65cab021ee0abfdcbdbe30f8e2aae4
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2022-12-03 03:05:38 +0200

    Create ChoosePayMethoudPage

---
+++ src/pages/Checkout/ChoosePayMethoudPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import ChoosePayMethoud from './../../components/Checkout/ChoosePayMethoud';
+
+const ChoosePayMethoudPage = () => {
+    return (
+        <Container style={{minHeight: '505px'}}>
+            <ChoosePayMethoud/>
+        </Container>
+    )
+}
+
+export default ChoosePayMethoudPage
