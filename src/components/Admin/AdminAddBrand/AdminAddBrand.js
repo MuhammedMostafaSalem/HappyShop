c5bb24b4ae1ac9324b2240820f7b614a0d2c297a
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2022-12-04 07:51:21 +0200

    Create AdminAddBrandComponent

---
+++ src/components/Admin/AdminAddBrand/AdminAddBrand.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import '../AdminAddBrand/AdminAddBrand.css'
+import { Col, Row } from 'react-bootstrap'
+import avatar from '../../../images/avatar.png'
+
+const AdminAddBrand = () => {
+    return (
+        <div>
+            <Row className="justify-content-start ">
+                <div className="admin-content-text pb-4">اضف ماركه جديده</div>
+                <Col sm="8">
+                    <div className="text-form pb-2">صوره الماركه</div>
+                    <img src={avatar} alt="" height="100px" width="120px"/>
+                    <input
+                        type="text"
+                        className="input-form d-block mt-3 px-3"
+                        placeholder="اسم الماركه"
+                    />
+                </Col>
+            </Row>
+            <Row>
+                <Col sm="8" className="d-flex justify-content-end ">
+                    <button className="btn btn-dark mt-2">حفظ التعديلات</button>
+                </Col>
+            </Row>
+        </div>
+    )
+}
+
+export default AdminAddBrand
