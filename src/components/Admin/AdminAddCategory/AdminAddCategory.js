052cef8581791823d43971f0e9cc7a0bc037f3f3
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2022-12-04 08:02:32 +0200

    Create AdminAddCategoryComponent

---
+++ src/components/Admin/AdminAddCategory/AdminAddCategory.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import '../AdminAddCategory/AdminAddCategory.css'
+import { Col, Row } from 'react-bootstrap';
+import avatar from '../../../images/avatar.png'
+
+const AdminAddCategory = () => {
+    return (
+        <div>
+            <Row className="justify-content-start ">
+                <div className="admin-content-text pb-4">اضافة تصنيف جديد</div>
+                <Col sm="8">
+                    <div className="text-form pb-2">صوره التصنيف</div>
+                    <img src={avatar} alt="" height="100px" width="120px"/>
+                    <input
+                        type="text"
+                        className="input-form d-block mt-3 px-3"
+                        placeholder="اسم التصنيف"
+                    />
+                </Col>
+            </Row>
+            <Row>
+                <Col sm="8" className="d-flex justify-content-end ">
+                    <button className="btn btn-dark mt-2">حفظ التعديلات</button>
+                </Col>
+            </Row>
+        </div>
+    )
+}
+
+export default AdminAddCategory
