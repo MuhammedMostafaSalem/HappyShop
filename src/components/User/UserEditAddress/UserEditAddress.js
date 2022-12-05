2726d14d04d7e7a23b2099e08fb982192b84f32e
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2022-12-05 11:28:15 +0200

    Create UserEditAddressComponent

---
+++ src/components/User/UserEditAddress/UserEditAddress.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Col, Row } from 'react-bootstrap'
+import '../UserEditAddress/UserEditAddress.css'
+
+const UserEditAddress = () => {
+    return (
+        <div>
+            <div className='admin-content-text'>تعديل العنوان</div>
+            <Row className='justify-content-start'>
+                <Col sm="8">
+                    <input
+                        type="text"
+                        className="input-form d-block mt-3 px-3"
+                        placeholder="تسمية العنوان مثلا(المنزل-العمل)"
+                    />
+                    <textarea
+                        className="input-form-area p-2 mt-3"
+                        rows="4"
+                        cols="50"
+                        placeholder="العنوان بالتفصيل"
+                    />
+                    <input
+                        type="text"
+                        className="input-form d-block mt-2 px-3"
+                        placeholder="رقم الهاتف"
+                    />
+                </Col>
+            </Row>
+            <Row>
+                <Col sm="8" className="d-flex justify-content-end">
+                    <button className="btn btn-dark mt-2 ">اضافة عنوان</button>
+                </Col>
+            </Row>
+        </div>
+    )
+}
+
+export default UserEditAddress
