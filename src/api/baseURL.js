cfe70004218cd9f25c34590fba5070088d35d4ab
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2022-12-12 06:28:16 +0200

    axios.create for BaseUrl

--- src/api/baseURL.js
+++ src/api/baseURL.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const BaseUrl = axios.create({baseURL:"http://127.0.0.1:8000"})
+const BaseUrl = axios.create({baseURL:"http://127.0.0.1:8000"});
 
-export default BaseUrl
+export default BaseUrl;
